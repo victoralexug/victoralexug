890d4d888974d3eb62497448bac74717e0aa8e0f
Victor Eze <victoralexu@gmail.com>
2023-08-21 03:24:11 +0100

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -53,6 +53,86 @@ window.onload = function() {
 };
 
 
+/*===== NAV BAR =====*/
+const nav = document.querySelector('.nav__list'),
+    navList = nav.querySelectorAll('li'),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll('.section'),
+    totalSection = allSection.length;
+
+    for(let i = 0; i<totalNavList; i++){
+        const a = navList[i].querySelector('a');
+         a.addEventListener('click', function(){
+            
+            removeBackSection();
+
+            for(let j=0; j<totalNavList; j++){
+                if(navList[j].querySelector('a').classList.contains('active')){
+                    addBackSection(j);
+                    //allSection[j].classList.add('back-section');
+                }
+                navList[j].querySelector('a').classList.remove('active');
+            }
+             this.classList.add('active')
+             showSection(this);
+         })
+    }
+
+    function removeBackSection(){
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove('back-section');
+        }
+    }
+
+    function addBackSection(num){
+        allSection[num].classList.add('back-section')
+    }
+
+     function showSection(element){
+        for(let i=0; i<totalSection; i++){
+            allSection[i].classList.remove('active');
+        }
+         const target = element.getAttribute('href').split('#')[1];
+         document.querySelector('#' + target).classList.add('active')
+     } 
+
+     function updateNAV(element){
+        for(let i=0; i<totalNavList; i++){
+            navList[i].querySelector('a').classList.remove('active');
+            const target = element.getAttribute('href').split('#')[1];
+            if(target === navList[i].querySelector('a').getAttribute('href').split('#')[1]){
+                navList[i].querySelector('a').classList.add('active');
+            }
+        }
+     }
+
+     //button # linking
+     document.querySelector('.about-me').addEventListener('click', function(){
+        const sectionIndex = this.getAttribute('data-section-index');
+        showSection(this);
+        updateNAV(this);
+        removeBackSection();
+        addBackSection(sectionIndex);
+     })
+     
+     document.querySelector('.hire-me').addEventListener('click', function(){
+        const sectionIndex = this.getAttribute('data-section-index');
+        showSection(this);
+        updateNAV(this);
+        removeBackSection();
+        addBackSection(sectionIndex);
+     })
+     
+     document.querySelector('.see-projects').addEventListener('click', function(){
+        const sectionIndex = this.getAttribute('data-section-index');
+        showSection(this);
+        updateNAV(this);
+        removeBackSection();
+        addBackSection(sectionIndex);
+     })
+     
+
+
 /*===== MENU SHOW =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
@@ -61,52 +141,74 @@ const showMenu = (toggleId, navId) =>{
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show')
+            for(let i=0; i<totalSection; i++){
+                allSection[i].classList.add('show');
+            }
         })
     }
 }
 showMenu('nav-toggle','nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll('.nav__link'),
+     close = document.querySelector('.close-button') 
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show')
+    for(let i=0; i<totalSection; i++){
+        allSection[i].classList.remove('show');
+    }
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+close.addEventListener('click', linkAction)
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-
-function scrollActive(){
-    const scrollY = window.pageYOffset
-
-    sections.forEach(current =>{
-        const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id')
-
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
-        }else{
-            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
-        }
-    })
-}
-window.addEventListener('scroll', scrollActive)
 
 /*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '60px',
-    duration: 2000,
-    delay: 200,
+// const sr = ScrollReveal({
+//     origin: 'side',
+//     distance: '60px',
+//     duration: 2000,
+//     delay: 200,
 //     reset: true
-});
+// });
 
-sr.reveal('.home__data, .skills__subtitle, .skills__text',{}); 
-sr.reveal('.home__img, .about__text, .skills__img',{delay: 500}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 300}); 
+// sr.reveal('.contact-title, .contact-sub-title',{delay: 100}); 
+// sr.reveal(' .about-text',{ delay: 200}); 
+// sr.reveal('.project, .skills__container',{interval: 300});
+// sr.reveal('.home__img, .personal-info',{delay: 500});
+
+// const tr = ScrollReveal({
+//     origin: 'top',
+//     distance: '60px',
+//     duration: 2000,
+//     delay: 200,
+//     reset: true
+// });
+
+// tr.reveal('.section-title, .home__data',{delay: 100}); 
+// tr.reveal('.home__social-icon, .skill-item, .contact-info-item, .service-item',{ interval: 200}); 
+// tr.reveal('.project-button, .contact__input',{interval: 300});
+// tr.reveal('.home__img, .contact__form',{delay: 500});
+
+/*===== DARK AND LIGHT MODE =====*/
+const dayNight = document.querySelector('.day-night');
+const body = document.body;
+dayNight.addEventListener('click', () =>{
+    dayNight.querySelector('i').classList.toggle('fa-sun');
+    dayNight.querySelector('i').classList.toggle('fa-moon');
+    document.body.classList.toggle('dark');
+})
+window.addEventListener('load', () => {
+    if(document.body.classList.contains('dark'))
+    {
+        dayNight.querySelector('i').classList.add('fa-sun')
+    }
+    else
+    {
+        dayNight.querySelector('i').classList.add('fa-moon')
+    }
+})
 
