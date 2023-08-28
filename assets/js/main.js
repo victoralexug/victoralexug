1e088ab6a630ff90bcae55a2249cc3ca671a4540
Victor Eze <victoralexu@gmail.com>
2023-08-28 13:22:21 +0100

    updated section position

--- assets/js/main.js
+++ assets/js/main.js
@@ -54,83 +54,84 @@ window.onload = function() {
 
 
 /*===== NAV BAR =====*/
-const nav = document.querySelector('.nav__list'),
-    navList = nav.querySelectorAll('li'),
-    totalNavList = navList.length,
-    allSection = document.querySelectorAll('.section'),
-    totalSection = allSection.length;
+// const nav = document.querySelector('.nav__list'),
+//     navList = nav.querySelectorAll('li'),
+//     totalNavList = navList.length,
+//     allSection = document.querySelectorAll('.section'),
+//     totalSection = allSection.length;
 
-    for(let i = 0; i<totalNavList; i++){
-        const a = navList[i].querySelector('a');
-         a.addEventListener('click', function(){
+//     for(let i = 0; i<totalNavList; i++){
+//         const a = navList[i].querySelector('a');
+//          a.addEventListener('click', function(){
             
-            removeBackSection();
-            if(screen.width > 860) {
-                for(let j=0; j<totalNavList; j++){
-                    if(navList[j].querySelector('a').classList.contains('active')){
-                        addBackSection(j);
-                        //allSection[j].classList.add('back-section');
-                    }
-                    navList[j].querySelector('a').classList.remove('active');
-                }
-            }
-             this.classList.add('active')
-             showSection(this);
-         })
-    }
+//             removeBackSection();
+//             if(screen.width > 860) {
+//                 for(let j=0; j<totalNavList; j++){
+//                     if(navList[j].querySelector('a').classList.contains('active')){
+//                         addBackSection(j);
+//                         //allSection[j].classList.add('back-section');
+//                     }
+//                     navList[j].querySelector('a').classList.remove('active');
+//                 }
+//             }
+//              this.classList.add('active')
+//              showSection(this);
+//          })
+//     }
 
-    function removeBackSection(){
-        for(let i=0; i<totalSection; i++){
-            allSection[i].classList.remove('back-section');
-        }
-    }
+//     function removeBackSection(){
+//         for(let i=0; i<totalSection; i++){
+//             allSection[i].classList.remove('back-section');
+//         }
+//     }
 
-    function addBackSection(num){
-        allSection[num].classList.add('back-section')
-    }
+//     function addBackSection(num){
+//         allSection[num].classList.add('back-section')
+//     }
 
-     function showSection(element){
-        for(let i=0; i<totalSection; i++){
-            allSection[i].classList.remove('active');
-        }
-         const target = element.getAttribute('href').split('#')[1];
-         document.querySelector('#' + target).classList.add('active')
-     } 
+//      function showSection(element){
+//         for(let i=0; i<totalSection; i++){
+//             allSection[i].classList.remove('active');
+//         }
+//          const target = element.getAttribute('href').split('#')[1];
+//          document.querySelector('#' + target).classList.add('active')
+//      } 
 
-     function updateNAV(element){
-        for(let i=0; i<totalNavList; i++){
-            navList[i].querySelector('a').classList.remove('active');
-            const target = element.getAttribute('href').split('#')[1];
-            if(target === navList[i].querySelector('a').getAttribute('href').split('#')[1]){
-                navList[i].querySelector('a').classList.add('active');
-            }
-        }
-     }
+//      function updateNAV(element){
+//         for(let i=0; i<totalNavList; i++){
+//             navList[i].querySelector('a').classList.remove('active');
+//             const target = element.getAttribute('href').split('#')[1];
+//             if(target === navList[i].querySelector('a').getAttribute('href').split('#')[1]){
+//                 navList[i].querySelector('a').classList.add('active');
+//             }
+//         }
+//      }
 
      //button # linking
-     document.querySelector('.about-me').addEventListener('click', function(){
-        const sectionIndex = this.getAttribute('data-section-index');
-        showSection(this);
-        updateNAV(this);
-        removeBackSection();
-        addBackSection(sectionIndex);
-     })
+   
+    //  document.querySelector('.about-me').addEventListener('click', function(){
+    //     const sectionIndex = this.getAttribute('data-section-index');
+    //     showSection(this);
+    //     updateNAV(this);
+    //     removeBackSection();
+    //     addBackSection(sectionIndex);
+    //  })
      
-     document.querySelector('.hire-me').addEventListener('click', function(){
-        const sectionIndex = this.getAttribute('data-section-index');
-        showSection(this);
-        updateNAV(this);
-        removeBackSection();
-        addBackSection(sectionIndex);
-     })
+    //  document.querySelector('.hire-me').addEventListener('click', function(){
+    //     const sectionIndex = this.getAttribute('data-section-index');
+    //     showSection(this);
+    //     updateNAV(this);
+    //     removeBackSection();
+    //     addBackSection(sectionIndex);
+    //  })
      
-     document.querySelector('.see-projects').addEventListener('click', function(){
-        const sectionIndex = this.getAttribute('data-section-index');
-        showSection(this);
-        updateNAV(this);
-        removeBackSection();
-        addBackSection(sectionIndex);
-     })
+    //  document.querySelector('.see-projects').addEventListener('click', function(){
+    //     const sectionIndex = this.getAttribute('data-section-index');
+    //     showSection(this);
+    //     updateNAV(this);
+    //     removeBackSection();
+    //     addBackSection(sectionIndex);
+    //  })
      
 
 
@@ -190,7 +191,7 @@ const sections = document.querySelectorAll('section[id]')
 
 function scrollActive(){
     const scrollY = window.pageYOffset;
-    if(screen.width <= 860) {
+    // if(screen.width <= 860) {
         //code goes 
         sections.forEach(current =>{
             const sectionHeight = current.offsetHeight
@@ -203,27 +204,11 @@ function scrollActive(){
                 document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
             }
         })
-    }
+    // }
 }
 window.addEventListener('scroll', scrollActive)
 
 /*===== SCROLL REVEAL ANIMATION =====*/
-
-    const sr = ScrollReveal({
-        origin: 'side',
-        distance: '60px',
-        duration: 2000,
-        delay: 200,
-        reset: true
-    });
-
-   
-    sr.reveal('.contact-title, .contact-sub-title',{delay: 100}); 
-    sr.reveal(' .about-text',{ delay: 200}); 
-    sr.reveal('.project, .skills__container',{interval: 300});
-    sr.reveal('.home__img, .personal-info',{delay: 500});
-
-
 const tr = ScrollReveal({
     origin: 'top',
     distance: '60px',
@@ -233,7 +218,7 @@ const tr = ScrollReveal({
 });
 
     tr.reveal('.section-title, .home__data, .success',{delay: 100}); 
-    tr.reveal('.home__social-icon, .skill-item, .contact-info-item,.logos img, .service-item',{ interval: 200}); 
+    tr.reveal('.skill-item, .contact-info-item,.logos img, .service-item',{ interval: 100}); 
     tr.reveal('.project-button, .contact__input',{interval: 300});
     tr.reveal('.home__img, .contact__form',{delay: 500});
 
@@ -243,3 +228,35 @@ const tr = ScrollReveal({
         document.getElementById('success').className += ' hidden';
     
     }, 5000);
+
+const lr = ScrollReveal({
+        origin: 'left',
+        distance: '60px',
+        duration: 2000,
+        delay: 200,
+        reset: true
+    });
+
+   
+    lr.reveal('.project-desc, .contact-title',{delay: 100}); 
+    lr.reveal('.home__social-icon',{ delay: 200}); 
+    lr.reveal('.project-left, .service-sub-title',{interval: 300});
+    lr.reveal('.personal-info',{delay: 500});
+
+
+
+const rr = ScrollReveal({
+        origin: 'right',
+        distance: '60px',
+        duration: 2000,
+        delay: 200,
+        reset: true
+    });
+
+   
+    rr.reveal('.contact-sub-title',{delay: 100}); 
+    rr.reveal('.about-text',{ delay: 200}); 
+    rr.reveal('.project-right, .service-title',{interval: 300});
+    rr.reveal('.home__img',{delay: 500});
+
+    
